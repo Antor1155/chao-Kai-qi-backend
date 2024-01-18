02f568791a21cf5dfbfe7b39f4aa9346e8a38989
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-19 02:25:46 +0600

    products.js of mongoose schema designed

--- SchemaDesign/products.js
+++ SchemaDesign/products.js
@@ -1,10 +1,61 @@
 const { Schema, model, models } = require("mongoose");
 
 const ProductSchema = new Schema({
-    name: String, 
-    email: String, 
-
-}, {timestamps: true})
+    productName: {
+      type: String,
+      trim: true,
+    },
+    coverName: {
+      type: String,
+      trim: true
+    },
+    brand:{
+        type: String,
+        trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    minimOrderQuantity: {
+      type: Number,
+      min: 1 // Enforce minimum order quantity if provided
+    },
+    pricePerUnit: {
+      type: Number,
+      min: 0 // Enforce non-negative price if provided
+    },
+    productSize: {
+      type: String,
+      trim: true
+    },
+    productGrossWeight: {
+      type: String,
+      trim: true
+    },
+    imageArray: [String],
+    mainImage: {
+      type: String,
+      trim: true
+    },
+    colors: {
+      type: {
+        [String]: { // Dynamic key for color names
+          name: String,
+          colorValue: String,
+          imgLink: String
+        }
+      }
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now
+    }
+  });
 
 const Products = models?.Order || model("Products", ProductSchema)
 
---
+++ example.js
@@ -0,0 +1,30 @@
+const schemaData = {
+    productName: "ipad mini 6",
+    coverName: "Snap Rotate style",
+    description: "",
+    minimOrderQuantity: 10,
+    pricePerUnit: 2.6,
+    productSize: "200 * 150 * 13 cm",
+    productGrossWeight: "198 g",
+    imageArray: ["/ProductImages/example-type/IPAD1.jpg", "/ProductImages/example-type/IPAD2.jpg", "/ProductImages/example-type/IPAD3.jpg", "/ProductImages/example-type/IPAD1.jpg","/ProductImages/example-type/IPAD2.jpg", "/ProductImages/example-type/IPAD3.jpg"],
+
+    mainImage: "/ProductImages/example-type/IPAD1.jpg",
+    colors: {
+        roseGold: {
+            name: "rose gold",
+            colorValue: "#B78E89",
+            imgLink: "/ProductImages/example-type/IPAD1.jpg"
+        },
+        navy: {
+            name: "navy",
+            colorValue: "#3B3B5F",
+            imgLink: "/ProductImages/example-type/IPAD2.jpg"
+        },
+        black: {
+            name: "black",
+            colorValue: "#303030",
+            imgLink: "/ProductImages/example-type/IPAD3.jpg"
+        }
+    },
+
+}
